115710bd667f6d46575bbe42395d976cbdfac1ed
Harley Trung <harley@bsa.edu.vn>
2016-04-11 14:24:43 +0700

    add CoreLocation

---
+++ DeviceFrameworksDemo/CoreLocationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  DeviceFrameworksDemo
+//
+//  Created by Harley Trung on 4/11/16.
+//  Copyright © 2016 coderschool.vn. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class CoreLocationViewController: UIViewController {
+    lazy var locationManager = CLLocationManager()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("setting locationManager")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 200
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension CoreLocationViewController: CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+//            let span = MKCoordinateSpanMake(0.1, 0.1)
+//            let region = MKCoordinateRegionMake(location.coordinate, span)
+//            mapView.setRegion(region, animated: false)
+            print("location ", location)
+        }
+    }
+}
+
